61f8a7c699aae5f2c7c4a6b547f76ece81515249
alamin-shanto <mdalaminshanto1993@gmail.com>
2025-12-12 15:34:22 +0600

    added payment model

---
+++ src/models/Payment.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface PaymentDocument extends Document {
+  booking: mongoose.Types.ObjectId;
+  stripeChargeId?: string;
+  amount: number;
+  status: "pending" | "succeeded" | "failed";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const PaymentSchema = new Schema<PaymentDocument>(
+  {
+    booking: { type: Schema.Types.ObjectId, ref: "Booking", required: true },
+    stripeChargeId: { type: String },
+    amount: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "succeeded", "failed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Payment ||
+  mongoose.model<PaymentDocument>("Payment", PaymentSchema);
